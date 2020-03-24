a0be22a7fb0294caea4dcb884cb157c21286b58b
Andre Entrekin <andre.entrekin@gmail.com>
2020-03-24 13:16:35 -0500

    Redux

---
+++ client/src/reducers/itemReducer.js
@@ -0,0 +1,23 @@
+import uuidv1 from 'uuid/v1'
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from '../actions/types'
+
+const initialState = {
+    items: [
+        { id: uuidv1(), name: 'Eggs' },
+        { id: uuidv1(), name: 'Milk' },
+        { id: uuidv1(), name: 'Steak' },
+        { id: uuidv1(), name: 'Candy' },
+
+    ]
+}
+
+export default function (state = initialState, action) {
+    switch (action.type) {
+        case GET_ITEMS:
+            return {
+                ...state
+            }
+        default:
+            return state;
+    }
+}
