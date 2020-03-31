c25f02fe0ddaaf6374ea063b0bccf6cb95e8807a
Andre Entrekin <andre.entrekin@gmail.com>
2020-03-31 15:12:42 -0500

    changed routes

--- routes/api/items.js
+++ routes/api/items.js
@@ -3,20 +3,20 @@ const router = express.Router();
 
 const Item = require('../../models/Item');
 
-router.get('/', (req, res) => {
+router.get('/api/items', (req, res) => {
     Item.find()
         .sort({ date: -1 })
         .then(items => res.json(items))
 })
 
-router.post('/', (req, res) => {
+router.post('/api/items', (req, res) => {
     const newItem = new Item({
         name: req.body.name
     })
     newItem.save().then(item => res.json(item));
 })
 
-router.delete('/:id', (req, res) => {
+router.delete('/api/items/:id', (req, res) => {
     Item.findById(req.params.id)
         .then(item => item.remove().then(() => res.json({ success: true })))
         .catch(err => res.status(404).json({ success: false }))
